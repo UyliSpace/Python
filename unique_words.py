58dfdeb7ea268dfc0b94202ba66603a4878df711
UyliSpace <fedchenko.uylia@gmail.com>
2017-04-05 17:52:12 +0300

    Create unique_words.py

---
+++ unique_words.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def unique_words():
+	file = open('Hroniki_Narnij_1.txt', encoding='utf-8')
+	text = file.read()
+	words = []
+	raw_text = set(text.split())
+	for word in raw_text:
+	    if word.isalpha():
+	        words.append(word) 
+
+	text = file.close()
+	print(words)
+	print('У цьому тексті {0} унікальних слів'.format(len(words)))
+
+
+
+if __name__ == '__main__':
+	unique_words()
