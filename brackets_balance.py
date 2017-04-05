a686441f387aa903827cf6309dcfe4709554a225
UyliSpace <fedchenko.uylia@gmail.com>
2017-04-05 17:48:12 +0300

    Create brackets_balance.py

---
+++ brackets_balance.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+line = '(((((((((({[2, 3]}))))))))))'
+
+
+def brackets_balance(text):
+	pairs = {'(': ')', '{': '}', '[': ']'}
+	counter = 0
+	for i in line:
+		if i in pairs.keys():
+			counter = counter + 1
+		#	print(counter)
+		elif i in pairs.values():
+		    counter = counter - 1
+		#    print(counter)
+	if counter == 0:
+	    print("Всі дужки закриті")
+	else:
+	    print("Не всі дужки закриті")     
+
+
+if __name__ == '__main__':
+	brackets_balance(line) 
+
+  	
+
